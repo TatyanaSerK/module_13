8ecafa6479fbfd9c2493fd2426dabb4387c7b582
TatyanaSerK <163410714+TatyanaSerK@users.noreply.github.com>
2024-07-26 13:16:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = '7361297831:AAGgvYRVs--JZkSRaoZIujfZrZUCUs10Hbc'
+api = ''
 bot = Bot(token= api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
